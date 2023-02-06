595cac29d5d7fccaab824363da9f97938fc0b4da
Noel Kampus <noel@kampus.ch>
2023-02-06 10:34:33 +0100

    footer links added
    -> color don't work

--- docs/js/footer.js
+++ docs/js/footer.js
@@ -6,20 +6,20 @@ class Footer extends HTMLElement {
               <div class="footer-items-wrapper">
                 <div class="footer-item-wrapper">
                     <p>Contact</p>
-                    <h4>hello@gramgram.ch</h4>
+                    <h4 class="footer-link"><a href="mailto: hello@gramgram.ch" target="_blank">hello@gramgram.ch</a></h4>
                 </div>
                 <div class="footer-item-wrapper">
                     <p>Overview</p>
-                    <h4>Product Design</h4>
-                    <h4>Consulting</h4>
-                    <h4>Social Media</h4>
-                    <h4>Community</h4>
+                    <h4 class="footer-link"><a href="${webRootPath}product-design.html">Product Design</a></h4>
+                    <h4 class="footer-link"><a href="${webRootPath}consulting.html">Consulting</a></h4>
+                    <h4 class="footer-link"><a href="${webRootPath}social-media.html">Social Media</a></h4>
+                    <h4 class="footer-link"><a href="${webRootPath}community.html">Community</a></h4>
                 </div>
                 <div class="footer-item-wrapper">
                     <p>Social Links</p>
-                    <h4><a href="https://instagram.com/gramgram.ch" target="_blank">Instagram</a></h4>
-                    <h4><a href="https://www.linkedin.com/in/gramgram/" target="_blank">LinkedIn</a></h4>
-                    <h4><a>Spotify</a></h4>
+                    <h4 class="footer-link"><a href="https://instagram.com/gramgram.ch" target="_blank">Instagram</a></h4>
+                    <h4 class="footer-link"><a href="https://www.linkedin.com/in/gramgram/" target="_blank">LinkedIn</a></h4>
+                    <h4 class="footer-link"><a href="">Spotify</a></h4>
                 </div>
               </div>
               <div class="below-footer-wrapper">
